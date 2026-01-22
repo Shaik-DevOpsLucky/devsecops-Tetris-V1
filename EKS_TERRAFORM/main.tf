d0374bf97fb4396711e3af39064e23835f01d575
Shaik Moulali <115640750+Shaik-DevOpsLucky@users.noreply.github.com>
2026-01-22 18:21:53 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -1,51 +1,104 @@
+# main.tf
+
+############################
+# IAM Role for EKS Cluster #
+############################
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "eks-cluster-cloud"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "example_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.example.name
+}
+
+####################
+# VPC and Subnets  #
+####################
 # Get default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-# Get public subnets in supported AZs for control plane
-data "aws_subnets" "eks_control_plane" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-
-  filter {
-    name   = "availability-zone"
-    values = ["us-east-1a", "us-east-1b"] # Use at least two supported AZs
-  }
-}
-
-# Optional: get all public subnets for worker nodes
-data "aws_subnets" "eks_nodes" {
+# Get public subnets for the cluster
+data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
 }
 
-# Cluster provision
+#########################
+# EKS Cluster Resource  #
+#########################
 resource "aws_eks_cluster" "example" {
   name     = "EKS_CLUSTER"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.eks_control_plane.ids
-    endpoint_private_access = false
-    endpoint_public_access  = true
+    subnet_ids = data.aws_subnets.public.ids
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example_AmazonEKSClusterPolicy,
   ]
 }
 
-# Node group
+####################################
+# IAM Role for EKS Worker NodeGroup #
+####################################
+resource "aws_iam_role" "example1" {
+  name = "eks-node-group-cloud"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.example1.name
+}
+
+resource "aws_iam_role_policy_attachment" "example_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.example1.name
+}
+
+resource "aws_iam_role_policy_attachment" "example_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.example1.name
+}
+
+##########################
+# EKS Node Group         #
+##########################
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
   node_group_name = "Node-cloud"
   node_role_arn   = aws_iam_role.example1.arn
-  subnet_ids      = data.aws_subnets.eks_nodes.ids
+  subnet_ids      = data.aws_subnets.public.ids
 
   scaling_config {
     desired_size = 3
@@ -56,8 +109,8 @@ resource "aws_eks_node_group" "example" {
   instance_types = ["t3.small"]
 
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.example_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example_AmazonEC2ContainerRegistryReadOnly,
   ]
 }
